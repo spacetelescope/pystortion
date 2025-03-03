bfb87f8b63e2747ab595c8f658b26567db9a6a4c
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-03-03 18:14:24 -0500

    MNT: Remove astropy test runner

--- pystortion/_astropy_init.py
+++ pystortion/_astropy_init.py
@@ -31,12 +31,6 @@ if not _ASTROPY_SETUP_:  # noqa
         ConfigurationDefaultMissingError,
         ConfigurationDefaultMissingWarning)
 
-    # Create the test function for self test
-    from astropy.tests.runner import TestRunner
-    test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
-    test.__test__ = False
-    __all__ += ['test']
-
     # add these here so we only need to cleanup the namespace at the end
     config_dir = None
 
